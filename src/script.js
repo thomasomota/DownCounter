d14d885c1ab18242889b196ecf99c1a28464a4d9
Thomas <thomas.ulvoen@gmail.com>
2017-08-13 13:36:47 +0200

    Removed all the colors

--- src/script.js
+++ src/script.js
@@ -4,27 +4,26 @@ var buttonsContainer = document.querySelector('.buttons');
 var output = document.querySelector('#output');
 var start = document.querySelector('#start');
 var reset = document.querySelector('#reset');
-var color_box = document.querySelectorAll('.color_box');
-
-var rainbow_rgb = "#000000";
-var frequency = 0;
-var sin_constant = 0.05;
+var timediff = document.querySelector('#timediff')
 
 function buttonsClickHandler(event) {
+
   var element = event.target;
   if(element.nodeName !== 'BUTTON') { return; }
   
   var number = element.dataset.number;
   var currentNumber = output.textContent;
   
-  if(currentNumber.length < 9){
+  if(currentNumber.length < 5){
 	  output.textContent = currentNumber === '0' ? number : currentNumber += number;
   }
 }
 
 function startClickHandler(event) {
-	frequency = 0;
-	
+
+  var startDate = Date.now();
+  var endDate = new Date();
+
   var interval = setInterval(function() {
 	  
     var currentNumber = output.textContent;
@@ -32,11 +31,17 @@ function startClickHandler(event) {
     if(currentNumber === '0') {
       clearInterval(interval);
 	  resetAll();
-      return;
+	  endDate = Date.now();
+	  return;
+
     }
     output.textContent = Number(currentNumber) - 1;
-	ChangeTextColor();
   }, 100);
+
+
+  timediff.textContent = Math.abs(startDate.getTime() - endDate.getTime());
+
+
 }
 
 function resetAll(){
@@ -46,40 +51,8 @@ function resetAll(){
 function resetClickHandler(event){
 	output.textContent = '0';
 	output.style.color = "#ffffff";
-	for(var i = 0; i < color_box.length; i++){
-		color_box[i].style.backgroundColor = "#262626"
-	}
 }
 
-function ChangeTextColor(){
-	updateRainbowRgbNum();
-	output.style.color = rainbow_rgb;
-	
-	for( var i = 0; i < color_box.length; i++){
-		color_box[i].style.backgroundColor = rainbow_rgb;		
-	}
-}
-
-function updateRainbowRgbNum(){
-	
-	var red   = Math.sin(frequency*sin_constant + 0) * 127 + 128;
-	var green = Math.sin(frequency*sin_constant + 2) * 127 + 128;
-	var blue  = Math.sin(frequency*sin_constant + 4) * 127 + 128;
-	
-	rainbow_rgb = rgbToHex(red,green,blue);
-	
-	if(frequency<120){
-		frequency += 1;
-	}
-	else{
-		frequency = 0;
-	}	
-}
-
- function rgbToHex(red, green, blue) {
-        var rgb = blue | (green << 8) | (red << 16);
-        return '#' + (0x1000000 + rgb).toString(16).slice(1)
-  }
 
 
 buttonsContainer.addEventListener('click', buttonsClickHandler);
